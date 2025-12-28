e77aab08bc655d78f7feb7ed82e711231eb3098f
Suvorrow <suvoraum@gmail.com>
2025-12-28 16:10:48 +0100

    Refine, improvement and refactor code in main.rs file

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
 mod core {
     pub mod routes;
+    pub mod chat_data;
 }
 use core::routes::Route;
 use dioxus::prelude::*;
+use dioxus::desctop;
+use crate::core::chat_data::ChatDataProvider;
+use tracing_subscriber;
 
-fn main() {
-    dioxus::desktop::launch_cfg(
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Initialize logging
+    tracing_subscriber::fmt::init();
+
+    // Launch desctop app with context
+    desktop::launch_cfg(
         app,
-        dioxus::desktop::Config::default()
+        desktop::Config::default()
             .with_window(
-                dioxus::desktop::WindowBuilder::new()
+                desktop::WindowBuilder::new()
                     .with_inner_size(764.0, 480.0)
-                    .with_title("Syggrel chat")
+                    .with_title("Syggrel Chat")
             )
     );
+
+    Ok(())
 }
